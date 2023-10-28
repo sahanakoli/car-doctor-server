070fce64da1f68bc54d3a87b7aa0cd19d55ce524
Sahana Koli <sahanasultanakoli@gmail.com>
2023-10-28 09:08:44 +0600

    delete a booking

--- index.js
+++ index.js
@@ -69,6 +69,15 @@ async function run() {
       res.send(result);
     });
 
+    
+
+    app.delete('/bookings/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
